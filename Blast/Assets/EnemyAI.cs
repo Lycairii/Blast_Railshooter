551dc6e455faeeee199e8152a7e65a8c7a7ec29e
Lycairii <gem.creed@hotmail.co.uk>
2024-01-16 16:44:59 +0000

    added enemy ai script
    added enemy ai script, to start adding code asap, for their guns and etc.

--- Blast/Assets/EnemyAI.cs
+++ Blast/Assets/EnemyAI.cs
@@ -1,17 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class EnemyAI : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    private void ActivateLasers()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
+    private void DeactivateLasers()
     {
         
     }
